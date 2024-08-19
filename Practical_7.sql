adc7c73c2c56f51b37f3ba5833df5a0c54db75ad
LyLe10 <121488636+LyLe10@users.noreply.github.com>
2024-08-19 23:26:56 +0700

    Update Practical_7.sql

--- Practical_7.sql
+++ Practical_7.sql
@@ -39,7 +39,47 @@ from cte
 where rnk=1 
 group by transaction_date, user_id
 -- Bài 5
-
+select user_id, tweet_date, round(avg(tweet_count) over(partition by user_id
+order by tweet_date rows between 2 preceding and current row ),2) as rolling_avg_3d
+from tweets;
 -- Bài 6
+with cte AS
+(select *, lag(transaction_timestamp) over(PARTITION BY
+merchant_id, credit_card_id, amount order by transaction_timestamp) as prev_transaction_time 
+from transactions)
+
+select count(transaction_id) as payment_count
+from cte 
+where extract(EPOCH from transaction_timestamp-prev_transaction_time)/60<=100;
 -- Bài 7
+with cte as(
+select category, product, sum(spend) as total_spend from product_spend
+where extract(year from transaction_date)=2022
+group by category, product),
+
+  cte2 as(
+select *,row_number() over(partition by category
+order by total_spend desc) as rnk
+from cte)
+
+select category, product, total_spend
+from cte2
+where rnk<=2
+
 -- Bài 8
+with cte AS
+(select a.artist_name, count(*) as num_of_times
+from global_song_rank g left join songs s 
+on g.song_id=s.song_id
+left join artists a on s.artist_id=a.artist_id
+where g.rank<=10
+group by a.artist_name),
+
+cte1 as 
+(select *, dense_rank() over(order by num_of_times desc) as artist_rank
+from cte)
+
+select artist_name, artist_rank
+from cte1
+where artist_rank<=5
+order by artist_name, artist_rank;
